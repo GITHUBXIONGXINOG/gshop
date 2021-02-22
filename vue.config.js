a3afb2eaa56d4e0746b01af6d626a9a8575267fe
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-02-22 16:20:42 +0800

    自动登录

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    devServer: {
+        open: false,//自动打开浏览器
+        hotOnly: true,//热更新
+        proxy: {
+            '/api': {// 匹配所有以 '/api'开头的请求路径
+                target: 'http://localhost:4000',// 代理目标的基础路径
+                changeOrigin: true,// 支持跨域
+                pathRewrite: {
+                    '^/api':''// 重写路径: 去掉路径中开头的'/api'
+                }
+            }
+        }
+    }
+}
